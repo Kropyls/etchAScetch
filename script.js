600517372189d8a14e1b98cef1590e0a5b65c992
Kropyls <Kropyls@gmail.com>
2022-03-27 20:12:30 -0700

    added reset button that adjusts pixels in grid

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-const container = document.getElementById('container')
-let boxes = []
-for(i=0;i<256;i++){
-  let div = document.createElement('div');
-  div.id = "d"+i;
-  div.className = 'eas-boxes'
-  boxes[i] = div;
-  container.appendChild(boxes[i]);
-  
+const container = document.getElementById('container');
+gridMaker(container, 16);
 
-  boxes[i].addEventListener("mouseover", function(e){
-    e.target.style.backgroundColor = "crimson";
+
+function gridMaker(container, numPerSide){
+  container.style.gridTemplateColumns = `repeat(${numPerSide}, 1fr)`;
+  let boxes = [];
+  numPerSide = numPerSide * numPerSide;
+  for(i=0;i<numPerSide;i++){
+    let div = document.createElement('div');
+    div.id = "d"+i;
+    div.className = 'eas-boxes'
+    boxes[i] = div;
+    container.appendChild(boxes[i]);
+    
+
+    boxes[i].addEventListener("mouseover", function(e){
+      e.target.style.backgroundColor = "crimson";
+    });
+    
+  }
+  boxes.forEach(element => {
+    element.clientWidth = element.clientHeight;
   });
-  
 }
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+
+
+const resetButton = document.getElementById('reset');
+resetButton.addEventListener('click',function(){
+  removeAllChildNodes(container);
+  function number(){
+    //fix decimal catching
+    let num = parseInt(prompt('Please enter desired squares per side of the etch-a-sketch.'));
+    if((num > 0) && (num < 101) && (num%1)==0)
+      return num;
+    else{
+      alert('unexpected input, try again');
+      return number();
+    }
+  }
+  gridMaker(container, number())
+})
